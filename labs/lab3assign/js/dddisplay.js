44059fdaf9a5a11c57acd813c5518af84cd32573
Devin (they/them) <jinipier@iu.edu>
2023-02-02 17:34:23 -0500

    data driven display

--- labs/lab3assign/js/dddisplay.js
+++ labs/lab3assign/js/dddisplay.js
@@ -1,19 +1,31 @@
 //define object
-
+let box = {
     //define height, width, x, y, color
+    height: 100,
+    width: 200,
+    x: 300,
+    y: 400
+}
 
 //define function with one argument
-
+function makeBox(thing){
+    //run fill color
+    fill(thing.color);
+    
     //take object and draw to screen
+    rect(thing.x, thing.y, thing.width, thing.height);
+    
+}
 
 function setup(){
     createCanvas(window.innerWidth, window.innerHeight);
+    //set objext color
+    box.color = color(100, 150, 200);
 
 }
 
-
 function draw(){
-
     //run function
-
+    makeBox(box);
+    
 }
