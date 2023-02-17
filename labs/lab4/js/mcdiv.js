2f0ffb27659c54910034a76ed14af789d55023e5
Devin (they/them) <jinipier@iu.edu>
2023-02-17 16:22:43 -0500

    mcdiv and update algorithm for over and out

--- labs/lab4/js/mcdiv.js
+++ labs/lab4/js/mcdiv.js
@@ -1,3 +1,11 @@
 // get inner html
 
+let existing = document.getElementById("mcdiv");
+
+
 // on click, change text in have mc at the beginning
+
+existing.onclick = function(){
+    existing.innerHTML = "mc" + existing.innerHTML;
+    
+}
--- labs/lab4/js/overandout.js
+++ labs/lab4/js/overandout.js
@@ -1,3 +1,7 @@
+// get elementId
+
+// set background to blue
+
 // on mouseover, change color to black
 
 //on mouseoff, chane color to blue
