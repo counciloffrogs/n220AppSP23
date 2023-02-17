6a0f82b9e3d39b73221d5a8a0003bd7a7de356fb
Devin (they/them) <jinipier@iu.edu>
2023-02-17 16:30:46 -0500

    over and out and updating base html files

--- labs/lab4/js/overandout.js
+++ labs/lab4/js/overandout.js
@@ -1,7 +1,17 @@
 // get elementId
+let squareDiv = document.getElementById("overout");
 
 // set background to blue
+squareDiv.style.backgroundColor = "#0000FF";
 
 // on mouseover, change color to black
+squareDiv.onmouseover = function(){
+    squareDiv.style.backgroundColor = "#000";
+
+}
 
 //on mouseoff, chane color to blue
+squareDiv.onmouseout = function(){
+    squareDiv.style.backgroundColor = "#0000FF";
+
+}
