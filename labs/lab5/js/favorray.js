18ac98a59a8bda164797b494ff4a8d2a4a1feeaf
Devin (they/them) <jinipier@iu.edu>
2023-02-23 15:38:36 -0500

    favorray

--- labs/lab5/js/favorray.js
+++ labs/lab5/js/favorray.js
@@ -1,7 +1,17 @@
 // bring in div with getid
 
+favorray = document.getElementById("fav");
+
+
 // create array of 5 cool things
+
+const coolThings = ["skating", "cake", "archery", "painting", "the zoo"];
+
 
 // for loop with i equal to 0, adding 1, up to length of array
 
-    // edit innerhtml to add break and element at i and ", is one of my favorite things"
+for(let i = 0; i < coolThings.length; i++){
+    // edit innerhtml to add break and element at i and , is one of my favorite things
+    favorray.innerHTML = favorray.innerHTML + coolThings[i] + ", is one of my favorite things" + "<br>";
+
+}
