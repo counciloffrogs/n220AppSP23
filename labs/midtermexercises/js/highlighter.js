13f4f82959b118c79b72fcb2e81bd500932b1865
Devin (they/them) <jinipier@iu.edu>
2023-02-23 17:41:55 -0500

    alg

---
+++ labs/midtermexercises/js/highlighter.js
@@ -0,0 +1,33 @@
+// bring in all 7 divs individually, and common id for 6 divs
+
+// set all 6 div background color
+
+// function onclick with div 1
+    // set all 6 divs to non-bold and base color
+    // set make the clicked div bold and highlight color
+    // bring inner html to top div
+
+// function onclick with div 2
+    // set all 6 divs to non-bold and base color
+    // set make the clicked div bold and highlight color
+    // bring inner html to top div
+
+// function onclick with div 3
+    // set all 6 divs to non-bold and base color
+    // set make the clicked div bold and highlight color
+    // bring inner html to top div
+
+// function onclick with div 4
+    // set all 6 divs to non-bold and base color
+    // set make the clicked div bold and highlight color
+    // bring inner html to top div
+
+// function onclick with div 5
+    // set all 6 divs to non-bold and base color
+    // set make the clicked div bold and highlight color
+    // bring inner html to top div
+
+// function onclick with div 6
+    // set all 6 divs to non-bold and base color
+    // set make the clicked div bold and highlight color
+    // bring inner html to top div
---
+++ labs/midtermexercises/js/rps.js
@@ -0,0 +1,21 @@
+// bring in all 4 divs and score div
+
+// onclick div guard
+    // subtract half a point from score
+
+// onclick div rock
+    // generate random 1-4
+    // else if computer scissors, add one to score
+    // else if computer paper, subtract one
+
+// onclick div paper
+    // generate random 1-4
+    // else if computer rock, add one to score
+    // else if computer scissors, subtract one
+
+// onclick div scissors
+    // generate random 1-4
+    // else if computer paper, add one to score
+    // else if computer rock, subtract one
+
+// change result div text to reflect score and results as a function
