1549ce93222c18c401e6831ad6e8b7eb56c4b762
Devin (they/them) <jinipier@iu.edu>
2023-02-23 15:11:31 -0500

    loopbig

--- labs/lab5/js/loopbig.js
+++ labs/lab5/js/loopbig.js
@@ -1,6 +1,14 @@
 // bring in div with get id
+let big = document.getElementById("bigloop");
+
+console.log(big.innerHTML);
+
 
 // create a for loop with i starting a 1000, adding 1000, until more than 5000
 
-// edit innerhtml to add a break and current i
-// note: may need to make i a string, will look up how
+for(let i = 1000; i <= 5000; i += 1000){
+    // edit innerhtml to add a break and current i
+    // note: may need to make i a string, will look up how
+    big.innerHTML = big.innerHTML + "<br>" + i.toString();
+    
+}
