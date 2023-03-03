8285be5c98e39ef1d660c4950b593e74e2e5e00f
Devin (they/them) <jinipier@iu.edu>
2023-03-02 22:16:45 -0500

    homework finish

--- labs/midtermexercises/js/highlighter.js
+++ labs/midtermexercises/js/highlighter.js
@@ -1,33 +1,149 @@
-// bring in all 7 divs individually, and common id for 6 divs
+// bring in all 7 divs individually
+disbox = document.getElementById("display");
+cheezIt = document.getElementById("cheez");
+buzz = document.getElementById("bee");
+dyno = document.getElementById("dino");
+wat = document.getElementById("water");
+die = document.getElementById("dice");
+light = document.getElementById("lamp");
 
 // set all 6 div background color
+disbox.style.backgroundColor = "#aaaaff";
+cheezIt.style.backgroundColor = "#aaffaa";
+buzz.style.backgroundColor = "#aaffaa";
+dyno.style.backgroundColor = "#aaffaa";
+wat.style.backgroundColor = "#aaffaa";
+die.style.backgroundColor = "#aaffaa";
+light.style.backgroundColor = "#aaffaa";
 
 // function onclick with div 1
+cheezIt.onclick = function(){
     // set all 6 divs to non-bold and base color
+    buzz.style.fontWeight = "normal";
+    dyno.style.fontWeight = "normal";
+    wat.style.fontWeight = "normal";
+    die.style.fontWeight = "normal";
+    light.style.fontWeight = "normal";
+    buzz.style.backgroundColor = "#aaffaa";
+    dyno.style.backgroundColor = "#aaffaa";
+    wat.style.backgroundColor = "#aaffaa";
+    die.style.backgroundColor = "#aaffaa";
+    light.style.backgroundColor = "#aaffaa";
+    
     // set make the clicked div bold and highlight color
+    cheezIt.style.fontWeight = "bolder";
+    cheezIt.style.backgroundColor = "#ffaaaa";
+
     // bring inner html to top div
+    disbox.innerHTML = cheezIt.innerHTML;
+}
 
 // function onclick with div 2
+buzz.onclick = function(){
     // set all 6 divs to non-bold and base color
+    cheezIt.style.fontWeight = "normal";
+    dyno.style.fontWeight = "normal";
+    wat.style.fontWeight = "normal";
+    die.style.fontWeight = "normal";
+    light.style.fontWeight = "normal";
+    cheezIt.style.backgroundColor = "#aaffaa";
+    dyno.style.backgroundColor = "#aaffaa";
+    wat.style.backgroundColor = "#aaffaa";
+    die.style.backgroundColor = "#aaffaa";
+    light.style.backgroundColor = "#aaffaa";
+    
     // set make the clicked div bold and highlight color
+    buzz.style.fontWeight = "bolder";
+    buzz.style.backgroundColor = "#ffaaaa";
+
     // bring inner html to top div
+    disbox.innerHTML = buzz.innerHTML;
+}
 
 // function onclick with div 3
+dyno.onclick = function(){
     // set all 6 divs to non-bold and base color
+    buzz.style.fontWeight = "normal";
+    cheezIt.style.fontWeight = "normal";
+    wat.style.fontWeight = "normal";
+    die.style.fontWeight = "normal";
+    light.style.fontWeight = "normal";
+    buzz.style.backgroundColor = "#aaffaa";
+    cheezIt.style.backgroundColor = "#aaffaa";
+    wat.style.backgroundColor = "#aaffaa";
+    die.style.backgroundColor = "#aaffaa";
+    light.style.backgroundColor = "#aaffaa";
+    
     // set make the clicked div bold and highlight color
+    dyno.style.fontWeight = "bolder";
+    dyno.style.backgroundColor = "#ffaaaa";
+
     // bring inner html to top div
+    disbox.innerHTML = dyno.innerHTML;
+}
 
 // function onclick with div 4
+wat.onclick = function(){
     // set all 6 divs to non-bold and base color
+    buzz.style.fontWeight = "normal";
+    dyno.style.fontWeight = "normal";
+    cheezIt.style.fontWeight = "normal";
+    die.style.fontWeight = "normal";
+    light.style.fontWeight = "normal";
+    buzz.style.backgroundColor = "#aaffaa";
+    dyno.style.backgroundColor = "#aaffaa";
+    cheezIt.style.backgroundColor = "#aaffaa";
+    die.style.backgroundColor = "#aaffaa";
+    light.style.backgroundColor = "#aaffaa";
+    
     // set make the clicked div bold and highlight color
+    wat.style.fontWeight = "bolder";
+    wat.style.backgroundColor = "#ffaaaa";
+
     // bring inner html to top div
+    disbox.innerHTML = wat.innerHTML;
+}
 
 // function onclick with div 5
+die.onclick = function(){
     // set all 6 divs to non-bold and base color
+    buzz.style.fontWeight = "normal";
+    dyno.style.fontWeight = "normal";
+    wat.style.fontWeight = "normal";
+    cheezIt.style.fontWeight = "normal";
+    light.style.fontWeight = "normal";
+    buzz.style.backgroundColor = "#aaffaa";
+    dyno.style.backgroundColor = "#aaffaa";
+    wat.style.backgroundColor = "#aaffaa";
+    cheezIt.style.backgroundColor = "#aaffaa";
+    light.style.backgroundColor = "#aaffaa";
+    
     // set make the clicked div bold and highlight color
+    die.style.fontWeight = "bolder";
+    die.style.backgroundColor = "#ffaaaa";
+
     // bring inner html to top div
+    disbox.innerHTML = die.innerHTML;
+}
 
 // function onclick with div 6
+light.onclick = function(){
     // set all 6 divs to non-bold and base color
+    buzz.style.fontWeight = "normal";
+    dyno.style.fontWeight = "normal";
+    wat.style.fontWeight = "normal";
+    die.style.fontWeight = "normal";
+    cheezIt.style.fontWeight = "normal";
+    buzz.style.backgroundColor = "#aaffaa";
+    dyno.style.backgroundColor = "#aaffaa";
+    wat.style.backgroundColor = "#aaffaa";
+    die.style.backgroundColor = "#aaffaa";
+    cheezIt.style.backgroundColor = "#aaffaa";
+    
     // set make the clicked div bold and highlight color
+    light.style.fontWeight = "bolder";
+    light.style.backgroundColor = "#ffaaaa";
+
     // bring inner html to top div
+    disbox.innerHTML = light.innerHTML;
+}
--- labs/midtermexercises/js/rps.js
+++ labs/midtermexercises/js/rps.js
@@ -1,21 +1,104 @@
 // bring in all 4 divs and score div
+r = document.getElementById("rock");
+p = document.getElementById("paper");
+s = document.getElementById("scissors");
+g = document.getElementById("guard");
+result = document.getElementById("resultBox");
 
-// onclick div guard
-    // subtract half a point from score
 
-// onclick div rock
-    // generate random 1-4
-    // else if computer scissors, add one to score
-    // else if computer paper, subtract one
-
-// onclick div paper
-    // generate random 1-4
-    // else if computer rock, add one to score
-    // else if computer scissors, subtract one
-
-// onclick div scissors
-    // generate random 1-4
-    // else if computer paper, add one to score
-    // else if computer rock, subtract one
+score = 0;
+you = ""
+comp = ""
 
 // change result div text to reflect score and results as a function
+boxupdate = function(){result.innerHTML = "Score: " + score + "<br>Last Round:<br> You Chose: " + you + "<br>Computer Chose: " + comp;}
+
+// onclick div guard
+g.onclick = function(){
+    // subtract half a point from score
+    score -= .5;
+    num = Math.floor(Math.random() * 4);
+    you = "guard";
+    if(num == 0){
+        comp = "rock"
+    } else if (num == 1){
+        comp = "paper"
+    } else if (num == 2){
+        comp = "scissors"
+    } else{
+        comp = "guard"
+    }
+    boxupdate();
+}
+
+// onclick div rock
+r.onclick = function(){
+    // generate random 1-4
+    num = Math.floor(Math.random() * 4);
+    // if computer scissors, add one to score
+    if(num == 2){
+        score += 1;
+    } else if(num == 1){
+        // else if computer paper, subtract one
+        score -= 1;
+    }
+    you = "rock";
+    if(num == 0){
+        comp = "rock"
+    } else if (num == 1){
+        comp = "paper"
+    } else if (num == 2){
+        comp = "scissors"
+    } else{
+        comp = "guard"
+    }
+    boxupdate();
+}
+
+// onclick div paper
+p.onclick = function(){
+    // generate random 1-4
+    num = Math.floor(Math.random() * 4);
+    // if computer rock, add one to score
+    if(num == 0){
+        score += 1;
+    } else if(num == 2){
+        // else if computer scissors, subtract one
+        score -= 1;
+    }
+    you = "paper";
+    if(num == 0){
+        comp = "rock"
+    } else if (num == 1){
+        comp = "paper"
+    } else if (num == 2){
+        comp = "scissors"
+    } else{
+        comp = "guard"
+    }
+    boxupdate();
+}
+
+// onclick div scissors
+s.onclick = function(){
+    // generate random 1-4
+    num = Math.floor(Math.random() * 4);
+    // if computer paper, add one to score
+    if (num == 1){
+        score += 1;
+    } else if (num == 0){
+        // else if computer rock, subtract one
+        score -= 1;
+    }
+    you = "scissors";
+    if(num == 0){
+        comp = "rock"
+    } else if (num == 1){
+        comp = "paper"
+    } else if (num == 2){
+        comp = "scissors"
+    } else{
+        comp = "guard"
+    }
+    boxupdate();
+}
