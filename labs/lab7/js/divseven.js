a5603d54718c933ae5ddb2abe352ccc877399310
Devin (they/them) <jinipier@iu.edu>
2023-03-30 17:16:44 -0400

    divseven

--- labs/lab7/js/divseven.js
+++ labs/lab7/js/divseven.js
@@ -1,5 +1,24 @@
+//get button and div
+let butt = document.getElementById("check");
+let numbox = document.getElementById("return");
+
 // function on button click
-// get input value
-// check input value is an integer
-// if its divisible by 7 update div
-// else update div false
+butt.onclick = function(){
+    // get input value
+    let inp = document.getElementById("numberbox").value;
+    console.log(inp);
+    console.log(parseInt(inp) % 7 == 0);
+
+    // check input value is an integer
+    if(isNaN(inp) == true){
+        alert("Must input a number");
+    } else{
+        // if its divisible by 7 update div
+        if(parseInt(inp) % 7 == 0){
+            numbox.innerHTML = "True";
+        }else{
+            // else update div false
+            numbox.innerHTML = "False";
+    }
+    }
+}
