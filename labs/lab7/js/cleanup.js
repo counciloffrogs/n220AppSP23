b50378590b4491e2bb5d16bc77a397049d57f8d3
Devin (they/them) <jinipier@iu.edu>
2023-03-30 17:22:39 -0400

    clean up

--- labs/lab7/js/cleanup.js
+++ labs/lab7/js/cleanup.js
@@ -1,4 +1,14 @@
+// get button and div
+let butt = document.getElementById("clean");
+let space = document.getElementById("return");
+
 // function at button click
-// get content of input
+butt.onclick = function(){
+    // get content of input
+    let inp = document.getElementById("stringbox").value;
 // replace # with nothing
+    inp = inp.replaceAll('#', '');
+
 // update div
+    space.innerHTML = inp;
+}
