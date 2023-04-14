235b404010d3e1711a173e1a55916b48891dbaf4
Devin (they/them) <jinipier@iu.edu>
2023-04-13 23:16:43 -0400

    flashcards

--- labs/lab9/js/flashcards.js
+++ labs/lab9/js/flashcards.js
@@ -1,3 +1,11 @@
 // bring in divs together
+let cardBoxes = document.querySelectorAll(".cards");
+
+for (let i = 0; i < cardBoxes.length; i++){
 // set listener for divs on click 
+    cardBoxes[i].addEventListener("click", function () {
     // change content based on data attribute
+    cardBoxes[i].innerHTML = cardBoxes[i].dataset.change;
+    }
+    )
+}
