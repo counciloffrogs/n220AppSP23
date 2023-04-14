11ead4238d9495a820354a5095b73e74752e9b3c
Devin (they/them) <jinipier@iu.edu>
2023-04-13 23:01:14 -0400

    color change

--- labs/lab9/js/colorChange.js
+++ labs/lab9/js/colorChange.js
@@ -1,4 +1,21 @@
 // bring in divs together
-// set initial colors to grey
-// set listener for divs on click 
+let colorBoxes = document.querySelectorAll(".colors");
+
+// set divs on click
+for (let i = 0; i < colorBoxes.length; i++) {
+    // set initial colors to grey
+    colorBoxes[i].style.backgroundColor = "#aaaaaa";
+  colorBoxes[i].addEventListener("click", function () {
+    
+    // console.log("clicked");
+
     // change color based on data attribute
+    if (colorBoxes[i].dataset.change == "red") {
+      colorBoxes[0].style.backgroundColor = "#aa5a5a";
+    } else if (colorBoxes[i].dataset.change == "green") {
+      colorBoxes[1].style.backgroundColor = "#aaf59a";
+    } else if (colorBoxes[i].dataset.change == "blue") {
+      colorBoxes[2].style.backgroundColor = "#3aaaaa";
+    }
+  });
+}
